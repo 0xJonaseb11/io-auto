ec1fe23b0ae3a4dcbcb039733cf6dabbc62d34cb
jonas-sebera <sebejaz99@gmail.com>
2023-12-01 01:57:35 +0200

    init server

--- server/server.js
+++ server/server.js
@@ -19,3 +19,8 @@ io.on('connection', (socket) => {
         console.log('User disconnected');
     });
 });
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
